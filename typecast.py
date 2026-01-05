73760ae60b6be3e1c71eb87d04e802e0cc08c890
divya <divyaparth2003@gmail.com>
2026-01-05 21:41:54 +0530

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,18 @@
+#explitic conversion
+string="12"
+number=10
+string_number = int(string)
+sum= number + string_number
+print(sum)
+
+#implisit typecasting
+
+d=24
+e=34.6
+
+print(type(d))
+print(type(e))
+sum =d+e
+print(sum)
+
+print(type(sum))
