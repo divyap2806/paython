0df13c7cb2affb0259e5d03fbd69b6ded6b201b3
divya <divyaparth2003@gmail.com>
2026-01-06 21:51:46 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,15 @@
+# #input from user 
+
+# a = input()
+# print("enter a:",a)
+
+# #in every time when we tack a input from the user is always a string and we have to change it in disire number if it is needed.
+
+# int (input("enter a number"))
+# float(input("enter a float number"))
+
+a = input("enter a number 1")
+b = input("enter a number 2")
+
+sum = int(a) + int(b)
+print(sum)
